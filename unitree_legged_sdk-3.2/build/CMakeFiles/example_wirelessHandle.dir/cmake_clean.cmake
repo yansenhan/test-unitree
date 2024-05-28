2101662f91c5a89a9c501c3fee1455b5a7611021
hanyansen <hanyansen@gmail.com>
2024-05-28 10:03:06 +0800

    update v3.2

---
+++ unitree_legged_sdk-3.2/build/CMakeFiles/example_wirelessHandle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_wirelessHandle.dir/examples/example_wirelessHandle.cpp.o"
+  "example_wirelessHandle"
+  "example_wirelessHandle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_wirelessHandle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
